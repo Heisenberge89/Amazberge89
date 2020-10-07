45171e844ab37bfb604c83768e4c79b6b36a2a22
Heisenberge89 <66557143+Heisenberge89@users.noreply.github.com>
2020-10-07 14:12:33 +0200

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,7 @@
+<?php
+//IMPOSTAZIONI DA MODIFICARE
+$bitlytoken = 'e917e634d8b6c14bf292eb43b2429bd305a108eb'; //inserisci qui dentro il token bitly [CAMBPO OBBLIGATORIO]
+$api = '1244559649:AAGrwj5d3hoKb24lqViP8vz3eC3Qq6GfLEc'; //inserisci l'api token del tuo bot telegram [CAMBPO OBBLIGATORIO]
+$chats = [12345678, -1000234567]; //inserisci qui dentro i chatID che possono usare il bot se vuoi.
+$ref = 'heisenberge0d-21'; //inserisci qui dentro il tuo tag amazon.
+$amzn = false; //Metti true per referrare anche i link amzn.to (sconsigliato). Consigliato lasciare false.
